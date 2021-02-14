7a818103f065350c185e02040044da0afb3aa989
Roman Stadler <rosta99x@gmx.ch>
2021-02-14 15:49:58 +0100

    Add files via upload

---
+++ cropMargins.py
@@ -0,0 +1,13 @@
+import subprocess
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def list_files(directory, extension):
+    return (f for f in os.listdir(directory) if f.endswith('.' + extension))
+
+pdfs = list_files(dir_path, "pdf")
+
+for pdf in pdfs:
+    print("editing "+str(pdf))
+    subprocess.call('pdf-crop-margins -u -s '+pdf, shell=True)
